d058031a27390970bf13007f3e838bd338ae2f50
hannahm63 <52358146+hannahm63@users.noreply.github.com>
2020-03-01 15:56:47 -0700

    rearranging and restyling portfolio. added favicon files

---
+++ assets/javascript/projects.js
@@ -0,0 +1,135 @@
+var projects = [
+    {
+        title: "GameOn",
+        description: "Lacus sed turpis tincidunt id aliquet risus feugiat in ante. Mi tempus imperdiet nulla malesuada.",
+        deployedLink: "https://game-on3.herokuapp.com/",
+        repoLink: "",
+        image: "https://via.placeholder.com/300",
+        imageAlt: "",
+        techUsed: ["HTML", "CSS", "JavaScript", "jQuery", "Node.js", "Express", "Axios", "Third-Party API", "SQL", "Sequelize"]
+    },
+    {
+        title: "Golf Pro",
+        description: "Lacus sed turpis tincidunt id aliquet risus feugiat in ante. Mi tempus imperdiet nulla malesuada.",
+        deployedLink: "",
+        repoLink: "",
+        image: "https://via.placeholder.com/300",
+        imageAlt: "",
+        techUsed: ["HTML", "CSS", "Firebase", "JavaScript", "jQuery", "Bootstrap", "AJAX", "Third-Party API"]
+    },
+    {
+        title: "Psychic Numbers",
+        description: "Tristique senectus et netus et malesuada fames ac.",
+        deployedLink: "https://hannahm63.github.io/psychic-game/",
+        repoLink: "https://github.com/hannahm63/psychic-game",
+        image: "https://via.placeholder.com/300",
+        imageAlt: "",
+        techUsed: ["HTML", "CSS", "JavaScript", "Bootstrap"]
+    },
+    {
+        title: "Word Guess Game",
+        description: "Turpis egestas integer eget aliquet nibh praesent tristique magna.",
+        deployedLink: "https://hannahm63.github.io/word-guess-game/",
+        repoLink: "https://github.com/hannahm63/word-guess-game",
+        image: "https://via.placeholder.com/300",
+        imageAlt: "",
+        techUsed: ["HTML", "CSS", "JavaScript", "Bootstrap"]
+    },
+    {
+        title: "Trivia!",
+        description: "Laoreet non curabitur gravida arcu ac tortor dignissim convallis aenean.",
+        deployedLink: "https://hannahm63.github.io/trivia-game/",
+        repoLink: "https://github.com/hannahm63/trivia-game",
+        image: "https://via.placeholder.com/300",
+        imageAlt: "",
+        techUsed: ["HTML", "CSS", "JavaScript", "jQuery", "Bootstrap"]
+    },
+    {
+        title: "Gif-Tastic Animals",
+        description: "Venenatis cras sed felis eget velit aliquet.",
+        deployedLink: "https://hannahm63.github.io/gif-tastic/",
+        repoLink: "https://github.com/hannahm63/gif-tastic",
+        image: "https://via.placeholder.com/300",
+        imageAlt: "",
+        techUsed: ["HTML", "CSS", "JavaScript", "jQuery", "Bootstrap", "AJAX", "Third Party API"]
+    },
+    {
+        title: "Train Time",
+        description: "Dictumst vestibulum rhoncus est pellentesque.",
+        deployedLink: "https://hannahm63.github.io/train-time/",
+        repoLink: "https://github.com/hannahm63/train-time",
+        image: "https://via.placeholder.com/300",
+        imageAlt: "",
+        techUsed: []
+    },
+    {
+        title: "Crystal Collector",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        deployedLink: "https://hannahm63.github.io/crystal-game/",
+        repoLink: "https://github.com/hannahm63/crystal-game",
+        image: "https://via.placeholder.com/300",
+        imageAlt: "",
+        techUsed: ["HTML", "CSS", "JavaScript", "jQuery", "Bootstrap"]
+    },
+    {
+        title: "News Scraper",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        deployedLink: "https://news-scraper-20.herokuapp.com/",
+        repoLink: "https://github.com/hannahm63/news-scraper",
+        image: "https://via.placeholder.com/300",
+        imageAlt: "",
+        techUsed: ["HTML", "CSS", "JavaScript", "jQuery", "Bootstrap", "MVC", "Node.js", "Express", "Axios", "Cheerio", "Morgan", "MongoDB", "Mongoose", "Handlebars"]
+    },
+    {
+        title: "Burger Tracker",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        deployedLink: "https://hannahm63.github.io/burger/",
+        repoLink: "https://github.com/hannahm63/burger",
+        image: "https://via.placeholder.com/300",
+        imageAlt: "",
+        techUsed: ["HTML", "CSS", "JavaScript", "jQuery", "Bootstrap", "MVC", "Node.js", "Express", "Handlebars"]
+    }
+];
+
+// ================================================================================
+// List of Tech Used for reference to maintain spelling consistency
+// ================================================================================
+// AJAX
+// Animate On Scroll (AOS)
+// API
+// Auth0
+// Axios
+// Bootstrap
+// Bulma
+// Canvas
+// Cheerio
+// CSS
+// ES6
+// ESLint
+// Express
+// Firebase
+// Handlebars
+// HTML
+// JavaScript
+// jQuery
+// Materialize
+// Media Queries
+// MongoDB
+// Mongoose
+// Morgan
+// MVC
+// MySQL
+// Node
+// NoSQL
+// NPM
+// Passport
+// PostgreSQL
+// React
+// React Hooks
+// Redux
+// REST
+// Sequelize
+// SQL
+// Third-perty API
+// ================================================================================
+// ================================================================================
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,4 +2,15 @@ $(document).ready(function () {
 
     console.log("hello");
 
+    $(".nav-item").on("click", function () {
+        $(".nav-item").removeClass("active");
+        $(this).addClass("active");
+
+        $('html,body').animate({
+            scrollTop: $("#projectSection").offset().top
+        },
+            'slow');
+
+    })
+
 });
