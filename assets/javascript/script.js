2a3e61804a623943e90ece3e0244dee9c7d66a77
hannahm63 <52358146+hannahm63@users.noreply.github.com>
2020-03-01 23:13:33 -0700

    started converting to single page and added some more styling

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,14 +6,29 @@ $(document).ready(function () {
         $(".nav-item").removeClass("active");
         $(this).addClass("active");
 
-        $('html,body').animate({
-            scrollTop: $("#projectSection").offset().top
-        },
-            'slow');
+        let aboutPage = $("#about-nav");
+        let projectsPage = $("#projects-nav");
+        let contactPage = $("#contact-nav");
+
+        if (aboutPage.hasClass("active")) {
+            $("#homePage").addClass("hide");
+        };
+
+        if (projectsPage.hasClass("active")) {
+            
+        };
+
+        if (contactPage.hasClass("active")) {
+            
+        };
 
     });
 
     $("#viewResume").on("click", function () {
+        window.open("./assets/pdf/Hannah_Melton_Resume.pdf");
+    });
+
+    $(".resume-nav").on("click", function () {
         window.open("./assets/pdf/Hannah_Melton_Resume.pdf");
     });
 
