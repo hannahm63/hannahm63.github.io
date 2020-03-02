eaefcdcfa1319b2203216da3a8b26e2e1453c066
hannahm63 <52358146+hannahm63@users.noreply.github.com>
2020-03-01 23:36:16 -0700

    added on click functions to hide or display elements based on active nav

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
 
-    console.log("hello");
-
     $(".nav-item").on("click", function () {
         $(".nav-item").removeClass("active");
         $(this).addClass("active");
@@ -10,25 +8,31 @@ $(document).ready(function () {
         let projectsPage = $("#projects-nav");
         let contactPage = $("#contact-nav");
 
+        console.log($(this).attr("id"));
+
         if (aboutPage.hasClass("active")) {
+            console.log("about Page active");
             $("#homePage").addClass("hide");
+            $("#aboutPage").removeClass("hide");
         };
 
         if (projectsPage.hasClass("active")) {
-            
+            console.log("project Page active");
         };
 
         if (contactPage.hasClass("active")) {
-            
+            console.log("contact Page active");
         };
 
     });
 
-    $("#viewResume").on("click", function () {
-        window.open("./assets/pdf/Hannah_Melton_Resume.pdf");
+    $(".navbar-brand").on("click", function () {
+        console.log("name clicked");
+        $("#aboutPage").addClass("hide");
+        $("#homePage").removeClass("hide");
     });
 
-    $(".resume-nav").on("click", function () {
+    $(".viewResume").on("click", function () {
         window.open("./assets/pdf/Hannah_Melton_Resume.pdf");
     });
 
